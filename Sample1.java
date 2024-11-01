62f9b0162b316f3d0dc1ab0f75a5914a4eee0c9d
Pradeep Jagarapu <117536420+PradeepJagarapu@users.noreply.github.com>
2024-11-01 11:59:12 +0530

    Update Sample1.java
    Datapro first chair

--- Sample1.java
+++ Sample1.java
@@ -2,4 +2,7 @@ public class Sample1{
     public void fun1(){
         System.out.println("fun1 created by srikar");
     }
+    public void fun2(){
+        System.out.println("fun2 created by pradeep");
+    }
 }
